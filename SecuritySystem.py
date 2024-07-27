199042916eab1cec32997e499e1294e55c7209a1
eReR98 <abruderer7@gmail.com>
2024-07-27 14:11:55 -0600

    Add frametime code and error handling

--- SecuritySystem.py
+++ SecuritySystem.py
@@ -1,16 +1,43 @@
 # This is the start of the security system code. Currently only captures an image
 
 import cv2
+import atexit
+import time
 
+
+# Define control variables here
+PrintFrameTime = False
+PrintQRVal = False
+
+prevTime = time.time()
+currTime = time.time()
+
+# Setup Code
 cam = cv2.VideoCapture(0)
 
-ret, frame = cam.read()
+def exitHandler():
+    print("exit command detected. Exiting...")
+    cam.release()
 
-if not ret:
-    print("error capturing image")
+atexit.register(exitHandler)
 
-cv2.imwrite("I_am_a_test_image.png", frame)
+# Program Loop
+while(True):
 
-cam.release()
+    ret, frame = cam.read()
+
+    currTime=time.time()
+    
+    if PrintFrameTime:
+        print("Frame time is " + str(currTime-prevTime))
+
+    prevTime=currTime
+
+    if not ret:
+        print("error capturing image")
+
+    #cv2.imwrite("I_am_a_test_image.png", frame)
+
+    
 
 
